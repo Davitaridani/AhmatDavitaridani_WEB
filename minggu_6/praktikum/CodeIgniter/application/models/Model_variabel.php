d2e638f250b3490c2c7581dd2cb3e8a0e1e4cb3e
Davitaridani <davitaridani@gmail.com>
2021-03-15 11:25:49 +0700

    E31191309

---
+++ minggu_6/praktikum/CodeIgniter/application/models/Model_variabel.php
@@ -0,0 +1,8 @@
+<?php 
+class Model_variabel extends CI_Model{
+	public $txt = "Hello World";
+	public $txt2 = "Hello World dari CI Model";
+
+}
+
+ ?>
---
+++ minggu_6/praktikum/CodeIgniter/application/views/variebel.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h2><?php echo $text;?></h2>
+	<h2><?php echo $text1;?></h2>
+</body>
+</html>
